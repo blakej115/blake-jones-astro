7c082f6904c564786406185b3f91f855b3b4239e
blakej115 <blakej115@gmail.com>
2024-04-28 20:02:50 -0400

    Switched logo mouse animation to a css only solution

---
+++ src/lib/hover-grid.ts
@@ -0,0 +1,40 @@
+export const gridDebug: boolean = false;
+
+export const gridBase: number = 21;
+
+const gridMultiplier: number = 2;
+
+const gridSubtractor: number = Math.floor(gridBase / 2);
+
+export const gridTotal: number[] = [
+  ...(Array(gridBase * gridBase).keys() as IterableIterator<number>),
+];
+
+export function gridX(x: number): number {
+  x = Math.floor(x / gridBase) - gridSubtractor;
+
+  if (gridDebug) return x;
+
+  return x * -gridMultiplier * (16 / 9);
+}
+
+export function gridY(y: number): number {
+  y = (y % gridBase) - gridSubtractor;
+
+  if (gridDebug) return y;
+
+  return y * gridMultiplier;
+}
+
+export const rotateClasses: string[] = gridTotal.map(
+  (i: number): string =>
+    `group-has-[>_div:first-of-type_>div:nth-of-type(${i + 1}):hover]:[transform:rotateX(${gridX(i)}deg)_rotateY(${gridY(i)}deg)]`,
+);
+
+const gridSafelist: string[] = [
+  ...rotateClasses,
+  `grid-cols-[repeat(${gridBase},minmax(0,1fr))]`,
+  `grid-rows-[repeat(${gridBase},minmax(0,1fr))]`,
+];
+
+export default gridSafelist;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
+import gridSafelist from "./src/lib/hover-grid";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [...gridSafelist],
   theme: {
     fontFamily: {
       sans: ["Figtree Variable", "sans-serif"],
@@ -10,6 +13,7 @@ export default {
       current: "currentColor",
       transparent: "transparent",
       white: "#fff",
+      black: "#000",
       blue: {
         DEFAULT: "#00b2ff",
         dark: "#253a4f",
